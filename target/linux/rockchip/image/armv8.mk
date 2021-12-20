112f8e439fd473bd4c4c1d7d6a51f0d8f69369a5
Zcracker <zcracker@163.com>
2021-12-20 13:19:28 +0800

    update radxa_rock-pi-4

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -31,12 +31,12 @@ define Device/pine64_rockpro64
 endef
 TARGET_DEVICES += pine64_rockpro64
 
-define Device/radxa_rock-pi-4a
+define Device/radxa_rock-pi-4
   DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi 4A
+  DEVICE_MODEL := ROCK Pi 4
   SOC := rk3399
-  SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
+  SUPPORTED_DEVICES := radxa,rockpi4
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
-TARGET_DEVICES += radxa_rock-pi-4a
+TARGET_DEVICES += radxa_rock-pi-4
